7db41e7a14113ffb76cd3a9b5d5dd80013bea6d3
Bo <gipnauy@gmail.com>
2021-06-18 13:27:05 -0700

    build structure

---
+++ src/main/java/SuanFaPeiLian/Session1_BinarySearch/Q1_LC278_FirstBadVersion/YongGe.java
@@ -0,0 +1,33 @@
+package SuanFaPeiLian.Session1_BinarySearch.Q1_LC278_FirstBadVersion;
+
+
+public class YongGe {
+    /*
+    You are a product manager and currently leading a team to develop a new product.
+    Unfortunately, the latest version of your product fails the quality check.
+    Since each version is developed based on the previous version,
+    all the versions after a bad version are also bad.
+    Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one,
+    which causes all the following ones to be bad. You are given an API bool isBadVersion(version)
+    which will return whether version is bad. Implement a function to find the first bad version.
+    You should minimize the number of calls to the API.
+    */
+    public int firstBadVersion(int n) {
+        int left = 1;//这里的left不是index，所以应该从1开始。这样后面就不需要check
+        int right = n;
+        int mid = 0;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    boolean isBadVersion(int version) {
+        return true;
+    }
+}
---
+++ src/test/java/SuanFaPeiLian/Session1_BinarySearch/Q1_LC278_FirstBadVersion/YongGeTest.java
@@ -0,0 +1,7 @@
+package SuanFaPeiLian.Session1_BinarySearch.Q1_LC278_FirstBadVersion;
+
+import static org.junit.Assert.*;
+
+public class YongGeTest {
+
+}
